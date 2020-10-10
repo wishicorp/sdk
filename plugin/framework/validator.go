9d6d583a6c93d6f6af55515ba47b8cf90c550910
liping <liping@yunzhishenghuo.com>
2020-10-10 16:40:44 +0800

    add api order

--- plugin/framework/validator.go
+++ plugin/framework/validator.go
@@ -1,7 +1,20 @@
 package framework
 
-import "github.com/go-playground/validator/v10"
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"strings"
+)
 
 func (b *Backend) Validate(in interface{}) error {
-	return validator.New().Struct(in)
+	err := validator.New().Struct(in)
+	if err != nil {
+		ferr := err.(validator.ValidationErrors)
+		var fs []string
+		for _, fieldError := range ferr {
+			fs = append(fs, fieldError.Field())
+		}
+		return fmt.Errorf("缺少参数: %s", strings.Join(fs, ", "))
+	}
+	return nil
 }
